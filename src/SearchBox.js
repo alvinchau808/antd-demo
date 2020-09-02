f7f20e55de2f18d614001e88d65f2c6481c88952
alvinchau808 <66266831+alvinchau808@users.noreply.github.com>
2020-09-02 22:56:03 +1000

    address auto complete google

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,4 +1,6 @@
+/* global google */
 import React, { handleChange } from 'react';
+import './SearchBox.css';
 
 class SearchBox extends React.Component {
     constructor(props) {
@@ -8,12 +10,28 @@ class SearchBox extends React.Component {
       }
 
     componentDidMount(){
-        var places = require('places.js');
-        var placesAutocomplete = places({
-            appId: 'plLOVX4P1KIW',
-            apiKey: '3a8a36e476f178d04d7a6934eb5c3408',
-            container: document.getElementById('address-input')
-        });
+      var options = {
+        componentRestrictions: {country: 'au'}
+      };
+      const input = document.getElementById("pac-input");
+      const autocomplete = new google.maps.places.Autocomplete(input, options);
+      autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+
+        if (!place.geometry) {
+          // User entered the name of a Place that was not suggested and
+          // pressed the Enter key, or the Place Details request failed.
+          window.alert("No details available for input: '" + place.name + "'");
+          return;
+        }
+      });
+
+      var places = require('places.js');
+      var placesAutocomplete = places({
+          appId: 'plLOVX4P1KIW',
+          apiKey: '3a8a36e476f178d04d7a6934eb5c3408',
+          container: document.getElementById('address-input')
+      });
     }
 
     handleChange(event) {
@@ -23,12 +41,19 @@ class SearchBox extends React.Component {
     
     render() {
         return (
-          <div>        
+          <div>
+            <div id="title">
+              Autocomplete search by Algolia
+            </div>
             <form>
               <label>
                 <input type="search" id="address-input" placeholder="Where are we going?" value={this.state.value} onChange={this.handleChange} />
               </label>
             </form>
+            <div id="title">
+              Autocomplete search by Google
+            </div>
+            <input id="pac-input" type="text" placeholder="Enter a location" />
           </div>
         );
     }
